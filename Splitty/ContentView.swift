f360b372b6e7933d0685e5b0f8015e56fa5c50af
Mahmoud Elshakoushy <MElshako@its.jnj.com>
2022-09-14 16:16:00 +0200

    Adding Done button to remove Keyboard

--- Splitty/ContentView.swift
+++ Splitty/ContentView.swift
@@ -9,37 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let students = ["Sheko", "Bsbs", "Lolo"]
+    @State private var checkAmout = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
     
-    @State private var tapCount = 0
-    @State private var name = ""
-    @State private var selectedStudent = "Sheko"
+    @FocusState private var amountIsFocused: Bool
     
+    let tipPercentages = [10, 15, 20, 0]
+    
+    var totalPerPerson: Double {
+        let peopleNumber = Double(numberOfPeople + 2)
+        let selectedTip = Double(tipPercentage)
+        
+        let tipvalue = checkAmout/100 * selectedTip
+        let grandTotal = checkAmout + tipvalue
+        let tipPerPerson = grandTotal / peopleNumber
+        return tipPerPerson
+    }
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Enter your name", text: $name)
-                    Text("Hello \(name)!")
-                    Button("Tap Count \(tapCount)") {
-                        tapCount += 1
-                    }
-                }
-                Section {
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                }
-                Section {
-                    Picker("Select your student", selection: $selectedStudent) {
-                        ForEach(students, id: \.self) {
-                            Text($0)
+                    TextField("Amount", value: $checkAmout, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
                         }
                     }
                 }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                }
             }
             .navigationTitle("Splitty")
-            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
