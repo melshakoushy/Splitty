374f0c082e9c4328f23210b7353aa8f34e694e23
Mahmoud Elshakoushy <MElshako@its.jnj.com>
2022-09-14 14:15:56 +0200

    Initial Commit

---
+++ Splitty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Splitty
+//
+//  Created by Mahmoud Elshakoushy on 14/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Splitty/SplittyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplittyApp.swift
+//  Splitty
+//
+//  Created by Mahmoud Elshakoushy on 14/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SplittyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
