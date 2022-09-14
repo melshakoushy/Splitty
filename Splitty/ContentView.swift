5b73a37e78e96c64055235eb91f854904fa73ffc
Mahmoud Elshakoushy <MElshako@its.jnj.com>
2022-09-14 15:03:27 +0200

    Creating a nav view

--- Splitty/ContentView.swift
+++ Splitty/ContentView.swift
@@ -9,15 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Section {
-                Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    Text("Hello, world!")
+                }
+                Section {
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+                }
             }
-            Section {
-                Text("Hello, world!")
-                Text("Hello, world!")
-            }
-
+            .navigationTitle("Splitty")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
