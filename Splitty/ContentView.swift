703445592a8aad27c2bdc450dc9e75f2afde4c49
Mahmoud Elshakoushy <MElshako@its.jnj.com>
2022-09-14 15:07:57 +0200

    Creating a new Button

--- Splitty/ContentView.swift
+++ Splitty/ContentView.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tapCount = 0
+    
     var body: some View {
         NavigationView {
             Form {
                 Section {
                     Text("Hello, world!")
+                    Button("Tap Count \(tapCount)") {
+                        tapCount += 1
+                    }
                 }
                 Section {
                     Text("Hello, world!")
